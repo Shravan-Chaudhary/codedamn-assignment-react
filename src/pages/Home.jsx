8e86daab17b8b1c53f4dc9bc877d4708ef3fafe8
Shravan <shravansinghchaudhary@gmail.com>
2024-01-17 16:28:44 +0530

    setup project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <section className='flex h-screen w-full items-center justify-center'>
+      <h1 className='text-5xl font-bold'>Code Damn Blog</h1>
+    </section>
+  )
+}
+
+export default Home
---
+++ src/pages/Posts.jsx
@@ -0,0 +1,11 @@
+import MaxWidthWrapper from '../components/MaxWidthWrapper'
+
+const Posts = () => {
+  return (
+    <MaxWidthWrapper>
+      <h1 className='text-5xl font-bold'>Posts</h1>
+    </MaxWidthWrapper>
+  )
+}
+
+export default Posts
